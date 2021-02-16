11b435305ec5d64db648cb9b46499806c3cf3bd7
Rattanak Leng <rleng@live.com>
2021-02-16 15:43:32 -0800

    upload intial files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { userAuthState } from 'react-firebase-hooks/auth';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
